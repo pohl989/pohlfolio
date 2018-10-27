8469a59031ccc93f4c5fda69ebe568f745b9d6a7
pohl989 <pohl989@gmail.com>
2018-10-27 15:59:05 -0600

    added navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,9 +2,49 @@ import React, { Component } from 'react';
 import { Menu,Icon } from 'semantic-ui-react';
 import { Link, withRouter } from 'react-router-dom';
 
-const NavBar = () => (
-  <h1>Home</h1>
-)
+
+class NavBar extends Component {
 
 
-export default NavBar
+render() {
+  return  (
+    <Menu.Menu position="center">
+    <header class="header-scrolled">
+   <div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">
+                Home
+          </Link>  
+        </li>
+        <li>
+          <Link to="/events" >
+            Events
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    </div>
+    </header>
+
+      </Menu.Menu>
+  );
+  }
+}
+
+
+export default withRouter(NavBar);
+
+
+// <div class="logo-wrap">
+//     <a class="logo" href="/">TableBooking</a>
+//   </div>
+
+
+// <Menu.Menu position='right'>
+// <Menu.Item
+  // name='Logout'
+  // onClick={() => dispatch(handleLogout(history))}
+// />
+// </Menu.Menu> 
