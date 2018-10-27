9b35d74f493166a572b0575766801095c8a59705
pohl989 <pohl989@gmail.com>
2018-10-27 15:54:08 -0600

    added semantic ui

---
+++ src/NavBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Menu,Icon } from 'semantic-ui-react';
+import { Link, withRouter } from 'react-router-dom';
+
+const NavBar = () => (
+  <h1>Home</h1>
+)
+
+
+export default NavBar
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react'
+
+const Home = () => (
+  <div>
+    <Button color={"red"}>Here is a button</Button>
+  </div>
+)
+
+export default Home
