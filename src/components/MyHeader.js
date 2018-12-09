7591f2c676e27880c2aba0d06d6fffbf96ab464f
pohl989 <pohl989@gmail.com>
2018-12-08 18:21:55 -0700

    switched out background image

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -7,31 +7,40 @@ import { Container,
 import styled from 'styled-components'
 import Header from '../styledComponents/Header.js'
 import logo from '../images/Pohl989_logo.png'
-import Background from '../images/houndstooth-pattern.png'
+import Background from '../images/volcano-pattern.png'
 
 
 const styles = {
-  width: "100%",
-  backgroundRepeat: "repeat-y",
-  backgroundImage: `url(${Background})`
+  boxStyles: {
+    width: "500px",
+    border: "2px solid red",
+    borderRadius: "20px",
+    margin: "5%",
+    padding: "5%",
+    position: "center",
+    backgroundColor: "rgba(76, 175, 80, 0.3)"
+  }
 }
 
 
 const Welcome = styled.div`
   background-image: url(${Background});
-  height:100vh`
+  height:100vh
+  overflow: auto`
 
 class MyHeader extends React.Component {
 
   render(){
     return(
       <Welcome>
-        <div style={{styles}} >
-          <Header style={{marginTop: '100px'}} as="h1" align="center"></Header>
-          <Header>Hello, I'm Ben Pohl.</Header>
-          <Header>I'm a fullstack Rails + React Developer</Header>
-          <Image src={logo} size='large' />
-          <Button>Click Me</Button>
+        <div style={styles.boxStyles}>
+          <div>
+            <Header style={{marginTop: '0px'}} as="h1" align="center"></Header>
+            <Header>Hello, I'm Ben Pohl.</Header>
+            <Header>I'm a fullstack Rails + React Developer</Header>
+            <Image src={logo} size='large' />
+            <Button>Click Me</Button>
+          </div>
         </div>
       </Welcome>
 
