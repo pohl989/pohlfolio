4175e0a4fb5e72e5f182d0348b26274fbd5dea9b
pohl989 <pohl989@gmail.com>
2019-01-13 13:31:55 -0700

    bug fixing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import styled from 'styled-components'
 import logo from '../images/Pohl989_logo.png'
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -8,47 +8,55 @@ class Project extends Component {
     const { title, description, subTitle, imageUrl, link } = this.props;
     return(
       <>
-        <ProjectCard href={link} >
-          <ProjectImage  src={imageUrl}/>
-          <div style={{
-            flexGrow: "1",
-            border: "none",
-            borderTop: "1px solid rgba(34,36,38,.1)",
-            background: "0 0",
-            margin: "0",
-            padding: "1em 1em",
-            boxShadow: "none",
-            fontSize: "1em",
-            borderRadius: "0",
-            textDecoration: "none"
-          }}
-          >
+        <a 
+          target="_blank"  
+          rel="noopener noreferrer"
+          href={link} 
+          style={{textDecoration: "none", display: "grid"}} 
+        >
+          <ProjectCard >
+            <ProjectImage  src={imageUrl}/>
             <div style={{
-              fontWeight: "700",
-              fontSize: "1.28571429em",
-              marginTop: "-.21425em",
-              lineHeight: "1.28571429em"
-            }}>
-              {title}
-            </div>
-            <div style={{
+              flexGrow: "1",
+              border: "none",
+              borderTop: "1px solid rgba(34,36,38,.1)",
+              background: "0 0",
+              margin: "0",
+              padding: "1em 1em",
+              boxShadow: "none",
               fontSize: "1em",
-              color: "rgba(0,0,0,.4)"
+              borderRadius: "0",
+              textDecoration: "none"
             }}
             >
-              {subTitle}
-            </div>
-            <div style={{
-              clear: "both",
-              color: "rgba(0,0,0,.68)"
+              <div style={{
+                fontWeight: "700",
+                fontSize: "1.28571429em",
+                marginTop: "-.21425em",
+                lineHeight: "1.28571429em",
+                color: "#111"
+              }}>
+                {title}
+              </div>
+              <div style={{
+                fontSize: "1em",
+                color: "rgba(0,0,0,.4)"
+              }}
+              >
+                {subTitle}
+              </div>
+              <div style={{
+                clear: "both",
+                color: "rgba(0,0,0,.68)"
 
-            }}
-            >
-              {description}
-            </div>
+              }}
+              >
+                {description}
+              </div>
 
-          </div>
-        </ProjectCard>
+            </div>
+          </ProjectCard>
+        </a>
       </>
     )
   }
