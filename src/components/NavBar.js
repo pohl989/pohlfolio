6db8a2e0078314419afe364a024ef2ada0adec11
pohl989 <pohl989@gmail.com>
2019-01-28 09:47:31 -0700

    add green eyeglass logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { media, colors } from '../styledComponents/Defaults'
 import logo from '../images/Pohl989_logo.png'
+import logoGreen from '../images/Pohl989_logo_green.png'
 
 const StyledMenu = styled.nav`
   box-shadow: 2px 3px 4px rgba(45,45,45,0.6), inset 2px 3px 4px rgba(45,45,45,0.9);
@@ -91,14 +92,31 @@ const MyFontAwesomeIcon = styled(FontAwesomeIcon)`
 
 class NavBar extends Component {
 
+  state = {
+    navHover: true
+  }
+
+  logoHeader = (hover) => {
+    if (hover) {
+      return (
+        <a href="#start" style={{display: "inline-flex", maxHeight: "60px", marginLeft: "2vw"}}>
+          <NavLogo src={logo} alt="Pohlfolio Logo" />
+        </a>        
+      )
+    } else {
+      return (
+        <a href="#start" style={{display: "inline-flex", maxHeight: "60px", marginLeft: "2vw"}}>
+          <NavLogo src={logoGreen} alt="Pohlfolio Logo" />
+        </a>  
+      )
+    }
+  }
+
   render() {
     return  (
       <StyledMenu id="navbar" >
-          <a href="#start" style={{display: "inline-flex", maxHeight: "60px", marginLeft: "2vw"}}>
-              <NavLogo src={logo} alt="Pohlfolio Logo" />
-            </a>  
-        <StyledUL>
-        
+          {this.logoHeader(this.state.navHover)}
+        <StyledUL>        
           <StyledLi style={{padding: '0px', marginBottom: '10px'}}>
           </StyledLi>
           <StyledLi>
