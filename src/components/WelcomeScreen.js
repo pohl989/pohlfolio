2aace414ef3f77753060fc2f9916cdf8fc17d738
pohl989 <pohl989@gmail.com>
2020-10-02 16:52:02 -0500

    updated my byline

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -32,7 +32,7 @@ function WelcomeScreen() {
       <WelcomeCard>
         <WelcomeLogo alt="Pohlfolio Logo" src={logo} size='small' style={{marginBottom: "15px", width: "150px", height: "auto", display: "block", position: "relative", marginRight: "auto", marginLeft: "auto"}}/>
         <Header size="large" align="center">Hello, I&apos;m Ben Pohl.</Header> 
-        <Header align="center" style={{marginBottom: '40px'}}>I&apos;m a Rails + React Developer</Header>
+        <Header align="center" style={{marginBottom: '40px'}}>I&apos;m a Rails + Javascript Developer</Header>
         <a href="#start">
           <BetterButton color='malibu'>View my work</BetterButton>
         </a>
