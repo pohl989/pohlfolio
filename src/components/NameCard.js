cf6e6211aa4f078284f9c04684292c2341c1faf9
pohl989 <pohl989@gmail.com>
2022-04-16 14:20:54 -0500

    add snew styled card

--- src/components/NameCard.js
+++ src/components/NameCard.js
@@ -1,23 +1,34 @@
 
 import React from 'react'
 import Header from '../styledComponents/Header'
-import {colors} from '../styledComponents/Defaults'
+import Fader from './Fader'
+import {StyledNameCard} from '../styledComponents/Card'
+
+const roles = [
+  'web developer',
+  'Rails on Rails guy',
+  'problem solver',
+  'JavaScript coder',
+  'pet parent',
+  'React lover',
+  'Marvel geek',
+  'Vue fanatic',
+  'dude who loves to code',
+]
+
 
 const NameCard = () => {
   return (
-    <div id="name-card" style={{
-      padding: '0px',
-      backgroundColor: colors.pink,
-      borderRadius: '0.8rem',
-      margin: '1rem',
-    }} >
-      <Header size="large" align="center" font="Obitron" bColor="white">HELLO</Header>
-      <Header align="center" font="Obitron" bColor="white">my name is</Header>
+    <StyledNameCard>
+      <div style={{padding: '0px 20px'}}>
+        <Header size="h1" align="center" font="Obitron" bColor="white">HELLO WORLD!</Header>
+      </div>
+      <Header align="center" font="Obitron" bColor="white" sub="true">my name is</Header>
       <Header size="large" align="center" bColor="neonGreen" font="Square Peg" background="white">BEN POHL</Header>
       <div style={{padding: '0px 20px'}}>
-        <Header align="center" style={{marginBottom: '40px'}} font="Obitron">I&apos;m a Rails + JavaScript Developer</Header>
+        <Header align="center" style={{marginBottom: '40px'}} font="Obitron">I&apos;m a <Fader texts={roles}></Fader></Header>
       </div>
-    </div>
+    </StyledNameCard>
   )
 }
 
--- src/styledComponents/Card.js
+++ src/styledComponents/Card.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
-import {media, colors} from './Defaults';
+import styled from 'styled-components'
+import {media, colors} from './Defaults'
 
-const cardShadow = 'inset 3px 4px 5px #000, 5px 6px 7px rgba(0, 0, 0, .3), inset 1px 2px 3px #fff';
-const cardShadow2 = 'inset 3px 4px 5px rgba(45, 45, 45, .2)';
+const cardShadow = 'inset 3px 4px 5px #000, 5px 6px 7px rgba(0, 0, 0, .3), inset 1px 2px 3px #fff'
+const cardShadow2 = 'inset 3px 4px 5px rgba(45, 45, 45, .2)'
 
-const borderRadius = '.8em;';
+const borderRadius = '.8em;'
 
 export const Cards = styled.div`
   justify-content: space-evenly;
@@ -12,7 +12,7 @@ export const Cards = styled.div`
   flex-wrap: wrap;
   box-sizing: border-box;
   align-items: stretch;
-`;
+`
 
 export const Card = styled.div`
   width: 80%;
@@ -20,7 +20,27 @@ export const Card = styled.div`
   border-radius: ${borderRadius};
   margin: 20px 10px;
   transition: 0.6s;
-`;
+`
+
+export const StyledNameCard = styled.div`
+  padding: 0px;
+  background-color: ${colors.pink};
+  border-radius: 0.8rem;
+  margin: 1rem;
+  min-width: 90%;
+  ${media.tablet`
+    min-width: 50%;
+    max-width: 1000px;
+    `}
+  ${media.desktop`
+    min-width: 50%;
+    max-width: 1000px;
+    `}
+  ${media.giant`
+    min-width: 50%;
+    max-width: 1000px;
+    `}
+`
 
 export const ProjectCard = styled(Card)`
   height: 90%;
@@ -41,7 +61,7 @@ export const ProjectCard = styled(Card)`
   &:hover{
     border: 8px solid ${colors.malibu};
   }
-`;
+`
 
 export const TextCard = styled(Card)`
   background-color: rgba(256, 256, 256, 0.1);
@@ -54,7 +74,7 @@ export const TextCard = styled(Card)`
   font-size: '1.6em';
   box-sizing: inherit;
   display: flex;
-`;
+`
 
 export const ProjectImage = styled.img`
   width: 100%;
@@ -63,7 +83,18 @@ export const ProjectImage = styled.img`
   box-shadow: ${cardShadow2};
   border-top-right-radius: .4em;
   border-top-left-radius: .4em;
-`;
+`
+
+export const NamedCard = styled.img`
+  width: 100%;
+  height: auto;
+  background-color: rgb(34,34,34);
+  box-shadow: ${cardShadow2};
+  border-top-right-radius: .4em;
+  border-top-left-radius: .4em;
+`
+
+
 export const WelcomeCard = styled(Card)`
   position: center;
   background-color: rgb(34,34,34);
@@ -72,8 +103,7 @@ export const WelcomeCard = styled(Card)`
   padding: 5%;
   ${media.desktop`
     margin: 20%;
-    `
-}
+    `}
   ${media.tablet`
     margin: 5px 7px;
     width: 90wh;
@@ -81,4 +111,4 @@ export const WelcomeCard = styled(Card)`
     justify-content: space-evenly;
     `
 }
-`;
+`
