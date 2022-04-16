9d2117d7a3930787f943c9a45b4586a2f15e356f
pohl989 <pohl989@gmail.com>
2022-04-16 14:21:22 -0500

    swap cards

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-import Header from '../styledComponents/Header';
-import logo from '../images/Pohl989_logo.png';
-import Background from '../images/dark-gravel.png';
-import {WelcomeCard} from '../styledComponents/Card';
-import {BetterButton} from '../styledComponents/Button';
+import React from 'react'
+import styled from 'styled-components'
+import Background from '../images/dark-gravel.png'
+import NameCard from './NameCard'
+
 
 const WelcomeBackground = styled.div`
   background-image: url(${Background});
@@ -12,33 +10,16 @@ const WelcomeBackground = styled.div`
   overflow: auto;
   display: flex;
   justify-content: center;
-  align-items: center;`;
-
-const WelcomeLogo = styled.img`
-  margin-bottom: "15px";
-  width: "150px";
-  height: "auto";
-  display: "block";
-  position: "relative";
-  margin-right: "auto";
-  margin-left: "auto;
-`;
+  align-items: center;`
 
 
 function WelcomeScreen() {
   return (
     <WelcomeBackground>
-      <WelcomeCard>
-        <WelcomeLogo alt="Pohlfolio Logo" src={logo} size='small' style={{marginBottom: '15px', width: '150px', height: 'auto', display: 'block', position: 'relative', marginRight: 'auto', marginLeft: 'auto'}}/>
-        <Header size="large" align="center">Hello, I&apos;m Ben Pohl.</Header>
-        <Header align="center" style={{marginBottom: '40px'}}>I&apos;m a Rails + JavaScript Developer</Header>
-        <a href="#start">
-          <BetterButton color='malibu'>View my work</BetterButton>
-        </a>
-      </WelcomeCard>
+      <NameCard />
     </WelcomeBackground>
 
-  );
+  )
 }
 
-export default WelcomeScreen;
+export default WelcomeScreen
